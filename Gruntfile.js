426af7d0042c464a2fc8321c09da4202ec7bd0a3
Jaden-Giordano <thejadenjack@gmail.com>
2017-03-12 19:32:52 -0600

    Added and implemented grunt!
    Also updated workflow, to allow for use of sass!

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Metadata.
+    meta: {
+      distPath: 'dist/',
+      srcPath: 'src/'
+    },
+
+    clean: {
+      dist: ['<%= meta.distPath %>/*']
+    },
+
+    sass: {
+      options: {
+        sourcemap: 'none',
+        style: 'expanded',
+        unixNewlines: true
+      },
+      core: {
+        src: '<%= meta.srcPath %>styles/**.scss',
+        dest: '<%= meta.distPath %>css/main.css'
+      },
+    },
+
+    copy: {
+      main: {
+        expand: true,
+        cwd: '<%= meta.srcPath %>app/',
+        src: '**',
+        dest: '<%= meta.distPath %>/'
+      },
+      package: {
+        expand: true,
+        src: 'package.json',
+        dest: '<%= meta.distPath %>'
+      },
+      fonts: {
+        expand: true,
+        cwd: '<%= meta.srcPath %>fonts/',
+        src: '**',
+        dest: '<%= meta.distPath %>fonts/'
+      }
+    },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: '*' // keep all important comments
+      },
+      main: {
+        src: '<%= meta.distPath %>css/main.css',
+        dest: '<%= meta.distPath %>css/main.min.css'
+      }
+    }
+  });
+
+  // Load the plugins
+  require('load-grunt-tasks')(grunt, {
+    scope: 'devDependencies'
+  });
+  require('time-grunt')(grunt);
+
+  // Tasks
+  grunt.registerTask('dist-css', ['sass', 'cssmin']);
+  grunt.registerTask('dist', ['clean', 'dist-css', 'copy']);
+
+  grunt.registerTask('default', ['dist']);
+};
